31ba82d41fa376eaa3774bcfab9cb405daa1d510
keyur154 <72492421+keyur154@users.noreply.github.com>
2021-01-07 18:34:47 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAogJj8boWA60dLs-8ikLbDzyi3PNeDmI0",
+    authDomain: "kwitter2-bbebd.firebaseapp.com",
+    databaseURL: "https://kwitter2-bbebd-default-rtdb.firebaseio.com",
+    projectId: "kwitter2-bbebd",
+    storageBucket: "kwitter2-bbebd.appspot.com",
+    messagingSenderId: "75728602223",
+    appId: "1:75728602223:web:18e6ad92462737fc22799a",
+    measurementId: "G-FPW1CCV99C"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
